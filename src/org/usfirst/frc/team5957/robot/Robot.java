38c395fded46636127482b676d0126eef2bc0a4c
Ben Miles <benmiles371@gmail.com>
2018-02-19 13:42:52 -0500

    Kp is good, finish adjusting Kd

--- src/org/usfirst/frc/team5957/robot/Robot.java
+++ src/org/usfirst/frc/team5957/robot/Robot.java
@@ -3,7 +3,6 @@ package org.usfirst.frc.team5957.robot;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Timer;
@@ -27,12 +26,18 @@ public class Robot extends IterativeRobot {
 	// PIDController frontRightPID;
 	// PIDController rearRightPID;
 
-	final double Kp = 0.05;
-	final double Ki = 0;
-	final double Kd = 0.0275;
+	final double Kp = 0.2125;
+	final double Kd = 0.4; // Kd = 6.4 when Kp = 0.3 can do 90 degrees turn
 	final double delay = 0.004;
-	
+
 	double target = 0;
+
+	double PCurrent = 0; // 90 - 0
+	double PLast = 0; // 90
+	double D = 0; // 0
+	double output = 0; // 0.9 - 0
+
+	double done = 0;
 
 	@Override
 	public void robotInit() {
@@ -57,6 +62,8 @@ public class Robot extends IterativeRobot {
 		joy = new Joystick(0);
 		gyro = new ADXRS450_Gyro();
 		time = new Timer();
+		gyro.reset();
+		gyro.calibrate();
 		// frontLeftPID = new PIDController(Kp, Ki, Kd, gyro, frontLeft);
 		// rearLeftPID = new PIDController(Kp, Ki, Kd, gyro, frontLeft);
 		// frontRightPID = new PIDController(Kp, Ki, Kd, gyro, frontLeft);
@@ -66,6 +73,10 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void robotPeriodic() {
+		PCurrent = target - gyro.getAngle(); // 90 - 0
+		PLast = PCurrent; // 90
+		D = PLast - PCurrent; // 0
+		output = (PCurrent * Kp) - (D * Kd); // 0.9 - 0
 	}
 
 	@Override
@@ -79,18 +90,14 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void autonomousInit() {
 		time.reset();
-		gyro.reset();
 		time.start();
-
 		double target = 90;
-
 		double PCurrent = target - gyro.getAngle(); // 90 - 0
 		double PLast = PCurrent; // 90
 		double D = PLast - PCurrent; // 0
 		double output = (PCurrent * Kp) - (D * Kd); // 0.9 - 0
 
 		while (output != 0 && gyro.getAngle() != target) {
-
 			if (output > 0.6) {
 				output = 0.6;
 			} else if (output < -0.6) {
@@ -104,70 +111,70 @@ public class Robot extends IterativeRobot {
 			PCurrent = target - gyro.getAngle();
 			D = PLast - PCurrent;
 			output = (PCurrent * Kp) - (D * Kd);
-
 		}
-
 	}
 
 	@Override
 	public void autonomousPeriodic() {
 	}
 
+	private void drive(double power) {
+		target = gyro.getAngle();
+		base.arcadeDrive(power, -output);
+	}
+
+	private void stop() {
+		target = gyro.getAngle();
+		base.arcadeDrive(0, -output);
+	}
+
+	private void turnToAngle(double angle) {
+		target = angle;
+
+	}
+
 	@Override
 	public void teleopInit() {
-		gyro.reset();
-		target = gyro.getAngle();
 	}
 
 	@Override
 	public void teleopPeriodic() {
-		double PCurrent = target - gyro.getAngle();
-		double PLast = PCurrent;
-		double D = PLast - PCurrent;
-		double output = (PCurrent * Kp) - (D * Kd);
-
-		// while (output != 0 && gyro.getAngle() != target) {
-		
-		 if (output > 0.6) {
-		 output = 0.6;
-		 } else if (output < -0.6) {
-		 output = -0.6;
-		
-		 }
-		
-		// }
-
-		if (joy.getRawAxis(4) < -0.05 || joy.getRawAxis(4) > 0.05) {
-			base.arcadeDrive(joy.getRawAxis(1), -joy.getRawAxis(4), true);
-			gyro.reset();
-		} else {
-			base.arcadeDrive(joy.getRawAxis(1), -output, true);
-		}
-		System.out.print("output: " + output);
-		System.out.print("target: " + target);
 	}
 
-	// public double getOutput(double angle) {
-	// double output;
-	//
-	// return output;
-	//
-	// }
+	@Override
+	public void testInit() {
+		gyro.reset();
+		gyro.calibrate();
+
+	}
 
 	@Override
 	public void testPeriodic() {
+		if (output != 0 && gyro.getAngle() != target) {
+
+			if (output > 0.6) {
+				output = 0.6;
+			} else if (output < -0.6) {
+				output = -0.6;
+
+			}
+			System.out.print(-output);
+			Timer.delay(delay);
+			PLast = PCurrent;
+			PCurrent = target - gyro.getAngle();
+			D = PLast - PCurrent;
+			output = (PCurrent * Kp) - (D * Kd);
+
+		}
+		if (time.get() > 0.01 && time.get() < 1) {
+			drive(0.4);
+		} else if (time.get() > 1.01 && time.get() < 1.05) {
+			stop();
+		} else if (time.get() > 1.06 && done == 0) {
+			target = target + 90;
+			done = 1;
+		}
+
 	}
 
-	// public void setDrivePID(double angle) {
-	//
-	// frontLeftPID.enable();
-	// rearLeftPID.enable();
-	// frontRightPID.enable();
-	// rearRightPID.enable();
-	// frontLeftPID.setSetpoint(angle);
-	// rearLeftPID.setSetpoint(angle);
-	// frontRightPID.setSetpoint(angle);
-	// rearRightPID.setSetpoint(angle);
-	//
-	// }
 }
